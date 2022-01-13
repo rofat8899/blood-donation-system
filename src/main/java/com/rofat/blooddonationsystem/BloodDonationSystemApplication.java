7344ee5d1c8525fe1d7cdc3e3c7e8350b16a3596
Sales Arofat <96236114+rofat8899@users.noreply.github.com>
2022-01-13 09:45:03 +0700

    Initial commit

---
+++ src/main/java/com/rofat/blooddonationsystem/BloodDonationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rofat.blooddonationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonationSystemApplication.class, args);
+	}
+
+}
